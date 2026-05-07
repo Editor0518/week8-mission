cac5347a329a3b378482a825f5165682c5d97065
편오 <108973850+Editor0518@users.noreply.github.com>
2026-05-07 22:18:19 +0900

    동적 데이터 정렬
    옵션C를 위주로 만들었으나 시간이 남아 옵션A도 구현

--- week8-mission/src/App.vue
+++ week8-mission/src/App.vue
@@ -3,36 +3,39 @@ import { ref } from 'vue';
 import MovieCard from './components/MovieCard.vue';
 
 const movies = ref([
-{
-  id: 1, title: '인셉션', rating: 9.5, likes: 0, genre: 'SF', isEditing: false,
-  poster: 'https://picsum.photos/seed/inception/300/450'
-},
-{
-  id: 2, title: '어바웃 타임', rating: 9.2, likes: 0, genre: '로맨스', isEditing: false,
-  poster: 'https://picsum.photos/seed/abouttime/300/450'
-},
-{
-  id: 3, title: '다크 나이트', rating: 9.8, likes: 0, genre: '액션', isEditing: false,
-  poster: 'https://picsum.photos/seed/darkknight/300/450'
-},
-{
-  id: 4, title: '기생충', rating: 8.9, likes: 0, genre: '드라마', isEditing: false,
-  poster: 'https://picsum.photos/seed/parasite/300/450'
-},
-{
-  id: 5, title: '라라랜드', rating: 6.8, likes: 0, genre: '로맨스', isEditing: false,
-  poster: 'https://picsum.photos/seed/lalaland/300/450'
-},
-{
-  id: 6, title: '인터스텔라', rating: 8.6, likes: 0, genre: 'SF', isEditing: false,
-  poster: 'https://picsum.photos/seed/interstellar/300/450'
-}
+  {
+    id: 1, title: '인셉션', rating: 9.5, likes: 0, genre: 'SF', isEditing: false,
+    poster: 'https://picsum.photos/seed/inception/300/450'
+  },
+  {
+    id: 2, title: '어바웃 타임', rating: 9.2, likes: 0, genre: '로맨스', isEditing: false,
+    poster: 'https://picsum.photos/seed/abouttime/300/450'
+  },
+  {
+    id: 3, title: '다크 나이트', rating: 9.8, likes: 0, genre: '액션', isEditing: false,
+    poster: 'https://picsum.photos/seed/darkknight/300/450'
+  },
+  {
+    id: 4, title: '기생충', rating: 8.9, likes: 0, genre: '드라마', isEditing: false,
+    poster: 'https://picsum.photos/seed/parasite/300/450'
+  },
+  {
+    id: 5, title: '라라랜드', rating: 6.8, likes: 0, genre: '로맨스', isEditing: false,
+    poster: 'https://picsum.photos/seed/lalaland/300/450'
+  },
+  {
+    id: 6, title: '인터스텔라', rating: 8.6, likes: 0, genre: 'SF', isEditing: false,
+    poster: 'https://picsum.photos/seed/interstellar/300/450'
+  }
 ]);
+
+const sortOrder = ref(0);
 
 //update 로직 (좋아요)
 const handleLike = (targetId) => {
   const movie = movies.value.find(m => m.id === targetId);
   if (movie) movie.likes++;
+  sortMovie();
 };
 //Delete 로직 (삭제)
 const handleDelete = (targetId) => {
@@ -59,15 +62,48 @@ const handleSave = (targetId, updatedData) => {
     movie.title = updatedData.title;
     movie.rating = updatedData.rating;
     movie.isEditing = false;
+    sortMovie();
   }
 };
+
+const sortByRating = () => {
+  if(sortOrder.value === 1)
+    sortOrder.value = 0; //한번 더 클릭한 경우 취소
+  else sortOrder.value = 1;
+  sortMovie();
+}
+
+const sortByLikes = () => {
+  if(sortOrder.value === 2)
+    sortOrder.value = 0; //한번 더 클릭한 경우 취소
+  else sortOrder.value = 2;
+  sortMovie();
+}
+
+const sortMovie = () => {
+  if(sortOrder.value === 1){
+    movies.value.sort((a, b) => b.rating - a.rating);
+  }
+  else if(sortOrder.value === 2){
+    movies.value.sort((a, b) => b.likes - a.likes);
+  }
+  else{
+    movies.value.sort((a, b) => a.id - b.id);
+  }
+}
 
 </script>
 <template>
   <div class="container">
     <h2>영화 리스트</h2>
     <template v-if="movies.length>0">
-      <p class="total-count">총 {{ movies.length }}개</p>
+      <div class="header-list">
+        <p class="total-count">총 {{ movies.length }}개</p>
+        <div class="btn-group">
+          <button class="btn sort-rating" :class="{'on': sortOrder===1}" @click="sortByRating">평점 높은 순</button>
+          <button class="btn sort-likes" :class="{'on': sortOrder===2}" @click="sortByLikes">좋아요 많은 순</button>
+        </div>
+      </div>
       <main class="movie-grid">
         <!--자식이 쓴 이벤트를 여기서 수신 대기-->
         <MovieCard
@@ -87,6 +123,7 @@ const handleSave = (targetId, updatedData) => {
     </template>
   </div>
 </template>
+
 <style scoped>
 .container { 
   width: 100%;
@@ -108,12 +145,48 @@ h2 {
   grid-template-columns: repeat(auto-fill, minmax(240px, 1fr)); 
   gap: 25px; 
 }
+
+.header-list{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 .no-movie{
   margin-top: 20%;
   text-align: center;
   color: gray;
 }
 .total-count{
-  color: gray;
+  color: rgb(191, 191, 191);
+  font-weight: bold;
 }
+
+.btn-group { 
+  display: flex; 
+  gap: 10px; 
+  justify-content: right; 
+  margin-bottom: 10px;
+}
+.btn {
+  padding: 10px 15px; 
+  cursor: pointer; 
+  border: none; 
+  border-radius: 6px; 
+  font-weight: bold; 
+  font-size: 0.9rem;
+  transition: opacity 0.2s;
+  background-color: #dcdcdc; 
+}
+.btn:hover { 
+  opacity: 0.8; 
+}
+.sort-likes.on{
+  background-color: #e74c3c; 
+  color: white;
+}
+.sort-rating.on{
+  background-color: #f39c12; 
+  color: white; 
+}
+
 </style>
